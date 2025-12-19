a675ccd63a358960b8f6901515498363d7a00bb9
Gabriele B <the.real.gabryy@gmail.com>
2025-12-19 05:47:47 +0100

    Init Commit - Hero

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gabriele Bolognese</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="CSS/style.css" />
+</head>
+<body>
+
+<header class="hero">
+    <div class="hero__inner">
+        <div class="hero__logo" aria-hidden="true">
+            <img src="Assets/IMG/logo-modified.png" alt="Gabriele Bolognese" class="hero__logo">
+        </div>
+
+        <div class="hero__text">
+            <div class="lockup">
+                <h1 class="title">TheRealG</h1>
+                <p class="subtitle">coding websites that SELL</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="button-container">
+        <button class="button">
+            View Portfolio
+        </button>
+    </div>
+</header>
+
+</body>
+</html>
