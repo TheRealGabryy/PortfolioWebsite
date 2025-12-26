8de561fdb23cdc609d9d66f6bbebcafc622ceb22
Gabriele B <the.real.gabryy@gmail.com>
2025-12-26 10:13:39 +0100

    Added About me Section

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Gabriele Bolognese</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet">
 
-    <link rel="stylesheet" href="CSS/style.css" />
+    <link rel="stylesheet" href="CSS/style.css"/>
+    <link rel="stylesheet" href="CSS/other.css">
 </head>
 <body>
 
@@ -33,6 +35,24 @@
         </button>
     </div>
 </header>
+
+<main class="main">
+    <section class="about-me">
+        <img src="Assets/IMG/logo-modified.png" alt="logo" class="hero__logo" style="margin: 50px;">
+        <div class="about-me__text">
+            <h2 class="about-me__title">Gabriele Bolognese</h2>
+            <p class="about-me__paragraph">
+                I build software systems with a strong focus on performance, structure, and long-term scalability.<br><br>
+
+                I started from low-level programming and system design, then moved into AI and machine learning to
+                understand how intelligent systems actually work. <br><br>
+
+                I design and implement algorithms from scratch in Java to master the fundamentals: neural networks, loss
+                functions, backpropagation, and optimization. then re-implement them in Pytorch for production use <br><br>
+            </p>
+        </div>
+    </section>
+</main>
 
 </body>
 </html>
